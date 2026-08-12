2ae98032b2e3bcc7845e2daa1455e75c171e8236
Raed Tanwar <raedtanwar72@gmail.com>
2026-08-12 21:17:04 +0530

    Time: 4 ms (91.64%) | Memory: 82.8 MB (70.38%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,25 @@
+// slow and fast pointer in array (same as linked list cycle)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow =0;
+        int fast=0;
+
+        while(true){
+            slow = nums[slow];
+            fast = nums[fast];
+            fast = nums[fast];
+
+            if(slow==fast){
+                slow=0;
+                
+                while(slow!=fast){
+                    slow = nums[slow];
+                    fast = nums[fast];
+                }
+                return slow;
+
+            }
+        }
+        
+    }
+}
